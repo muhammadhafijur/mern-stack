b43bb46d6aa8eb2c391b9e77cfe1ba23d95275a1
MD Shimul <mdshemul480@gmail.com>
2021-08-14 10:30:43 +0600

    adding new data to the data.js

--- js/data.js
+++ js/data.js
@@ -7,6 +7,30 @@ const sum = (a, b) => a + b
 console.log(sum(2, 6)) // prints 8    
 `,
     },
+    {
+      title: "Object Property Assignment",
+      content: `
+const a = 2
+const b = 5
+const obj = { a, b }
+// Before es6:
+// obj = { a: a, b: b }
+console.log(obj)
+// prints = { a: 2, b: 5 } 
+      `,
+    },
+    {
+      title: "Object Function Assignment",
+      content: `
+const obj = {
+  a: 5,
+  b() {
+    console.log('b')
+  }
+  }
+obj.b() // prints "b"
+      `,
+    },
   ],
 };
 
