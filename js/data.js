881c1989b2b7afed20c7509eaef35ae5b9b71da4
MD Shimul <mdshemul480@gmail.com>
2021-08-14 10:27:52 +0600

    adding data

---
+++ js/data.js
@@ -0,0 +1,13 @@
+const codeData = {
+  es6: [
+    {
+      title: "Arrow Function",
+      content: `
+const sum = (a, b) => a + b
+console.log(sum(2, 6)) // prints 8    
+`,
+    },
+  ],
+};
+
+export default codeData;
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import data from "./data.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(data);
+});
